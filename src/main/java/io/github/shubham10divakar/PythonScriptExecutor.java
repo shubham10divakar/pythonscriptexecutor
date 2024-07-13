8964413747775ddf4b9043f361d0773de5e083b2
shubham10divakar <shubham.divakar@gmail.com>
2024-07-13 12:15:17 +0530

    Initial commit

---
+++ src/main/java/io/github/shubham10divakar/PythonScriptExecutor.java
@@ -0,0 +1,22 @@
+package io.github.shubham10divakar;
+
+public class PythonScriptExecutor {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero");
+        }
+        return a / b;
+    }
+}
